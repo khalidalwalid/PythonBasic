104558682be142443525bcf4e0d46b32393de0de
Wildan <33023197+khalidalwalid@users.noreply.github.com>
2020-08-13 12:48:07 +0700

    Add files via upload

---
+++ List, Tuple, Set, Dictionary - LATIHAN.py
@@ -0,0 +1,10 @@
+# Python3
+# [LATIHAN] List, Tuple, Set, Dictionary
+
+print('==========================================================')
+print('List, Tuple, Set, Dictionary')
+print('\n')
+print('1. Jelaskan perbedaan list, tuple, set, dan dicionaries!')
+print('2. Apakah perbedaan diantara set dan frozenset?')
+print('3. Apa yang akan terjadi jika dalam sebuah kunci pada dictionaries diberikan lebih dari 1 nilai?')
+print('4. Buatlah sebuah kamus bahasa inggris-indonesia menggunakan konsep dictionaries sehingga pengguna dapat mengetahui arti dari bahasa inggris yang ingin dicarinya!')
